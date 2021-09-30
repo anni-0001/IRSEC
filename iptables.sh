dc15f7b33684a84e8626cb3fdaa7e2ec9ff36bc9
anni-0001 <amc1100@rit.edu>
2021-09-29 23:48:29 -0400

    mangle exsists...

--- iptables.sh
+++ iptables.sh
@@ -1,39 +1,43 @@
 #!/bin/sh
 
-
-admins = "@"
-
-
-# allows all services breifely while configuring
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT DROP
-
-#local
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+#admins = ''
 
 #flushes preset rules
+echo "---flushing---"
+iptables -t mangle -F
+iptables -t mangle -X
 iptables -F
+iptables -X
+
+# allows all services breifely while configuring
+iptables -t mangle -P INPUT DROP
+iptables -t mangle -P FORWARD DROP
+iptables -t mangle -P OUTPUT ACCEPT
+
+
+#local
+echo "---allow loopback---"
+iptables -t mangle -A INPUT -i lo -j ACCEPT
+iptables -t mangle -A OUTPUT -o lo -j ACCEPT
 
 #open port 80 HTTP 
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -t mangle -A INPUT -p tcp --dport 80 -j ACCEPT
 
 
 #SSH
 
 #open port 22 SSH
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -t mangle -A INPUT -p tcp --dport 22 -j ACCEPT
 #start logging ssh when session is started
 #grep to sort through /var/log/kern.log
 iptables -I INPUT -p tcp --dport 22 -j LOG --log-level 0 --log-prefix "SSH_IN___"
 # allows 4 ssh attempts by IP every 3 mins
-iptables -A INPUT -p tcp -m tcp --dport 22 -m state --state NEW -m recent --set --name DEFAULT --rsource
-iptables -A INPUT -p tcp -m tcp --dport 22 -m state --state NEW -m recent --update --seconds 180 --hitcount 4 --name DEFAULT --rsource -j DROP
-iptables -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
+iptables -t mangle -A INPUT -p tcp -m tcp --dport 22 -m state --state NEW -m recent --set --name DEFAULT --rsource
+iptables -t mangle -A INPUT -p tcp -m tcp --dport 22 -m state --state NEW -m recent --update --seconds 180 --hitcount 4 --name DEFAULT --rsource -j DROP
+iptables -t mangle -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
 
 # DoS attack protection
-iptables -A INPUT -p tcp --dport 80 -m limit --limit 20/minute --limit-burst 50 -j ACCEPT
+iptables -t mangle -A INPUT -p tcp --dport 80 -m limit --limit 20/minute --limit-burst 50 -j ACCEPT
 
 # add redirect rules for ports 465 and 587 (to let postfix run on different ports)
 iptables -t nat -A PREROUTING -p tcp --dport 465 -j REDIRECT --to-ports 25
@@ -42,12 +46,13 @@ iptables -t nat -A PREROUTING -p tcp --dport 587 -j REDIRECT --to-ports 25
 
 
 #reset the default policies to stop all incoming and forward requests
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
+iptables -t mangle -P INPUT DROP
+iptables -t mangle -P FORWARD DROP
 
 
 
 #reject any other traffic after all rules
-iptables -A INPUT -j reject
-iptables -A INPUT -j reject
+iptables -t mangle -A INPUT -j reject
+iptables -t mangle -A INPUT -j reject
+
 
--- iptables_p.sh
+++ iptables_p.sh
@@ -1,32 +1,39 @@
 #!bin/bash
 
+#flushes preset rules
+echo "---flushing---"
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -F
+iptables -X
+
 
 # allows all services breifely while configuring
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT DROP
+echo "default input"
+iptables -t mangle -P INPUT ACCEPT
+
+iptables -t mangle -P OUTPUT ACCEPT
+
 
 #local
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-
-#flushes preset rules
-iptables -F
-
+echo "---allow loopback---"
+iptables -t mangle -A INPUT -i lo -j ACCEPT
+iptables -t mangle -A OUTPUT -o lo -j ACCEPT
 
 
 #add redirect rules for ports 465 and 587 (to let postfix run on different ports)
+echo "---postfix---"
 iptables -t nat -A PREROUTING -p tcp --dport 465 -j REDIRECT --to-ports 25
 iptables -t nat -A PREROUTING -p tcp --dport 587 -j REDIRECT --to-ports 25
 
 
 
 #reset the default policies to stop all incoming and forward requests
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
+iptables -t mangle -P INPUT DROP
+iptables -t mangle -P FORWARD DROP
 
 
 
 #reject any other traffic after all rules
-iptables -A INPUT -j reject
-iptables -A INPUT -j reject
+iptables -t mangle -A INPUT -j reject
+iptables -t mangle -A INPUT -j reject
